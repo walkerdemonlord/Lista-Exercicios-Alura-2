722411a3c4ee5294fe12604ea0ed79fb7459d6d1
Walker <166620151+walkerdemonlord@users.noreply.github.com>
2024-07-08 14:37:58 -0300

    inicial 2

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+let textoH1 = document.getElementById('alterar-texto-h1');
+    textoH1.innerHTML = 'Jogo do Número Secreto';
+
+let paragrafo = document.querySelector('p');
+    paragrafo.innerHTML = 'Escolha um número entre 0 e 10:';
+
+function verificarChute() {
+        console.log('botão acionado!');
+    }
