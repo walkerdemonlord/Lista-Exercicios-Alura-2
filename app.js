780d61fb5ce8d5e82c14efedd2569cffef80bc36
Walker <166620151+walkerdemonlord@users.noreply.github.com>
2024-07-08 16:11:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 
+let numeroSecreto = gerarNumeroAleatorio ();
 
-let textoH1 = document.getElementById('alterar-texto-h1');
-    textoH1.innerHTML = 'Jogo do Número Secreto';
+exibirTexto('h1', 'Jogo do Número da Sorte');
+exibirTexto('p', 'Escolha um número entre 0 e 10:');
 
-let paragrafo = document.querySelector('p');
-    paragrafo.innerHTML = 'Escolha um número entre 0 e 10:';
+function exibirTexto (tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function gerarNumeroAleatorio () {
+    return parseInt(Math.random() * 10 + 1); // precisa retornar uma info
+} // parseInt para arredondar o número (inteiro)
 
 function verificarChute() {
-        console.log('botão acionado!');
-    }
+    console.log(numeroSecreto);
+} // exibe no console o número random
